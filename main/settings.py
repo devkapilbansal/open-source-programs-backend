90f13479206adf2cd6546e1f070cd8a72d143691
sankalp <sankalp123427@gmail.com>
2021-02-12 14:09:18 +0530

    Added GitHub Actions workflow to run tests
    Added tests.yml to run test written in this project on GitHub workflow.
    
    Changed databases in settings to use psql on GITHUB_WORKFLOW.
    
    Fixes #53

--- main/settings.py
+++ main/settings.py
@@ -112,16 +112,30 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "osp",
-        'USER': "osp",
-        'PASSWORD': "osp",
-        'HOST': "localhost",
-        'PORT': 5432,
+if os.environ.get("GITHUB_WORKFLOW"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": "postgres",
+            "USER": "postgres",
+            "PASSWORD": "osp",
+            "HOST": "localhost",
+            "PORT": 5432,
+        }
     }
-}
+
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": "osp",
+            "USER": "osp",
+            "PASSWORD": "osp",
+            "HOST": "localhost",
+            "PORT": 5432,
+        }
+    }
+
 
 
 # Password validation
